e20df8c9895bbffe831d61e6e2e7f704531c84de
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 00:21:36 +0200

    [content]: created header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import './styles/global.css'
+import Header from './components/header'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Header />
+    </>
+  )
 }
 
 export default App
