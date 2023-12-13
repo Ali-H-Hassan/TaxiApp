2487494070c651284ca6e3dc6ab369bf95820c77
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 15:37:32 +0200

    [docs]: added documentation to message component

--- frontend/src/components/message/index.jsx
+++ frontend/src/components/message/index.jsx
@@ -1,12 +1,22 @@
 import './index.css'
 
-export default function Index({ children, position }) {
+/**
+ * message component represents the message when sent in the echat
+ *
+ * @param position is the message on the left or right side
+ * @returns
+ *
+ * ```jsx
+ * <Message position={'right'} time={'12:30 am'}>text</Message>
+ * ```
+ */
+export default function Index({ children, position, time }) {
   return (
     <div className={`msg-container flex-start ${position === 'right' && 'flex-end'}`}>
       <div className={`msg ${position === 'right' ? 'right' : 'left'}`}>
         <div className={`msg-text`}>{children}</div>
         <span className={`msg-time ${position === 'right' ? 'flex-start' : 'flex-end'}`}>
-          12:33 am
+          {time}
         </span>
       </div>
     </div>
