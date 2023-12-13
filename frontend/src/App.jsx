d862b144dad31fd85c2603878c0e342e277f6a02
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 12:27:18 +0200

    [routes]: added react-router-dom and home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/global.css'
+import Header from './components/header'
+import Footer from './components/footer'
+import Home from './pages/home'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="*" element={<>404 page not found</>} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
