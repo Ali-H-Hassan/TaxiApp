550f304bb7b6d127c631123799944ffb4fbd659d
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 22:17:22 +0200

    [core]: added multi form

--- frontend/src/pages/auth/register/index.jsx
+++ frontend/src/pages/auth/register/index.jsx
@@ -1,3 +1,47 @@
+import '../index.css'
+import { useState } from 'react'
+import Button from '../../../components/buttons'
+import useMultistepForm from '../../../components/multi-form/core.jsx'
+import UserType from './user-type.jsx'
+
 export default function Index() {
-  return <div>register</div>
+  const [credentials, setCredentials] = useState({ email: '', password: '', type: '' })
+
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([
+      <UserType updateFields={setCredentials} />,
+      <div>hi</div>,
+      <div>2</div>
+    ])
+
+  function handleRegister(e) {
+    e.preventDefault()
+    console.log(credentials)
+
+    if (!isLastStep) return next()
+    alert('Successful Account Creation')
+  }
+
+  return (
+    <main className="auth-page">
+      <form className="auth-card" onSubmit={handleRegister}>
+        <h1 className="auth-card-title">Register</h1>
+
+        {step}
+        <div className={'auth-card-nav'}>
+          {!isFirstStep && (
+            <>
+              <Button variant={'secondary'} onClick={back} type="button">
+                Back
+              </Button>
+
+              <Button variant={isLastStep ? 'primary' : 'secondary'} type="submit">
+                {isLastStep ? 'Register' : 'Next'}
+              </Button>
+            </>
+          )}
+        </div>
+      </form>
+    </main>
+  )
 }
