8bfa348c51752bc902616081ae8af66afbd9fe1c
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-14 01:04:50 +0200

    [docs]: added documentation

--- frontend/src/util/index.js
+++ frontend/src/util/index.js
@@ -1,13 +1,31 @@
+/**
+ * get a value by a key from local storage
+ *
+ * @param key
+ * @returns
+ */
 export function getlocal(key) {
   const value = window.localStorage.getItem(key)
 
   return value
 }
 
+/**
+ * set a value to local storage
+ *
+ * @param key
+ * @returns
+ */
 export function setlocal(key, val) {
   window.localStorage.setItem(key, val)
 }
 
+/**
+ * remove a value from local storage
+ *
+ * @param key
+ * @returns
+ */
 export function removelocal(key) {
   window.localStorage.removeItem(key)
 }
