cae9c01cdcdc78f1bb7e318f11f63512bc79cdf0
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-14 00:50:22 +0200

    [routes]: added iser routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './components/footer'
 import Home from './pages/home'
 import Login from './pages/auth/login'
 import Register from './pages/auth/register'
+import UserPage from './pages/p/user'
+import DriverPage from './pages/p/driver'
 
 function App() {
   return (
@@ -16,6 +18,13 @@ function App() {
           <Route path="/" index element={<Home />} />
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/register" element={<Register />} />
+          <Route path="*" element={<>404 page not found</>} />
+        </Routes>
+
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/p/user" element={<UserPage />} />
+          <Route path="/p/driver" element={<DriverPage />} />
           <Route path="*" element={<>404 page not found</>} />
         </Routes>
 
