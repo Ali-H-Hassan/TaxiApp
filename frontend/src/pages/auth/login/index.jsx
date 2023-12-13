31f9260cdfb48a4ef6bfc5b3d76bca98dbbdb04b
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 20:04:00 +0200

    [content]: added login

--- frontend/src/pages/auth/login/index.jsx
+++ frontend/src/pages/auth/login/index.jsx
@@ -1,3 +1,39 @@
+import '../index.css'
+import { Link } from 'react-router-dom'
+import Input from '../../../components/input'
+import Button from '../../../components/buttons'
+
 export default function Index() {
-  return <div>login</div>
+  return (
+    <main className="auth-page">
+      <form className="auth-card">
+        <h1 className="auth-card-title">Login</h1>
+
+        <div className="auth-card-content">
+          <Input
+            label={'Email:'}
+            placeHolder={'mohamad@gmail.com'}
+            onChange={() => {}}
+            type="email"
+          />
+          <Input
+            label={'Password:'}
+            placeHolder={'************'}
+            onChange={() => {}}
+            type="password"
+          />
+          <p className="auth-card-content-p">
+            Don’t have an account?{' '}
+            <Link to={'/auth/register'} className="underlined">
+              Register here.
+            </Link>
+          </p>
+        </div>
+
+        <Button variant={'primary'} className="auth-card-btn">
+          Login
+        </Button>
+      </form>
+    </main>
+  )
 }
