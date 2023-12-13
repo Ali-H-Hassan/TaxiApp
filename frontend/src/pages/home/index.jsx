e79cdbdb8b1c116dac9291b75abe1d885afa1fe2
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 15:35:24 +0200

    [styles]: added styling to message component

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,5 @@
 import './index.css'
-import Message from '../../components/message'
 
 export default function Index() {
-  return (
-    <div>
-      <Message position={'left'}>hello ash jsa djhasdkjf kjadsf</Message>
-      <Message position={'right'}>hello jadhf kshdf ahsdk fhsdlkfha kdfh</Message>
-    </div>
-  )
+  return <div>home</div>
 }
