cdbae831dc81c8dfab5da16ad0892ebf01de8a6a
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-13 20:05:09 +0200

    [core]: added optional className as prop to button

--- frontend/src/components/buttons/index.jsx
+++ frontend/src/components/buttons/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './index.css'
 
-const Button = ({ onClick = () => {}, children, variant }) => {
+const Button = ({ onClick = () => {}, children, variant, className = '' }) => {
   return (
     <button
-      className={`button-container ${
+      className={`button-container ${className} ${
         variant === 'primary'
           ? 'primary'
           : variant === 'secondary'
