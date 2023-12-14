9efe105b1a6513335a6a75e2ec4bef60a9d24dbb
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-12-14 11:23:07 +0200

    [core]: get input values from edit profile

--- frontend/src/pages/p/edit-profile/index.jsx
+++ frontend/src/pages/p/edit-profile/index.jsx
@@ -1,14 +1,22 @@
 import './index.css'
 import Label from '../../../components/input'
 import Button from '../../../components/buttons'
+import { useState } from 'react'
 
 const EditProfile = () => {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    first_name: '',
+    last_name: '',
+    phone_number: ''
+  })
+
   const handleCancel = () => {
     console.log('Cancelled')
   }
 
   const handleSave = () => {
-    console.log('Saved')
+    console.table(userInfo)
   }
 
   return (
@@ -16,9 +24,42 @@ const EditProfile = () => {
       <div className="edit-profile-content">
         <div className="title">Edit Profile</div>
         <div className="form-section">
-          <Label label="First Name" value="Patrick" />
-          <Label label="Last Name" value="Star" />
-          <Label label="Contact Number" value="+961 12 345 678" />
+          <Label
+            label="Contact Number"
+            value={userInfo.email}
+            onChange={(e) => {
+              setUserInfo((prev) => {
+                return { ...prev, email: e.target.value }
+              })
+            }}
+          />
+          <Label
+            label="First Name"
+            value={userInfo.first_name}
+            onChange={(e) => {
+              setUserInfo((prev) => {
+                return { ...prev, first_name: e.target.value }
+              })
+            }}
+          />
+          <Label
+            label="Last Name"
+            value={userInfo.last_name}
+            onChange={(e) => {
+              setUserInfo((prev) => {
+                return { ...prev, last_name: e.target.value }
+              })
+            }}
+          />
+          <Label
+            label="Contact Number"
+            value={userInfo.phone_number}
+            onChange={(e) => {
+              setUserInfo((prev) => {
+                return { ...prev, phone_number: e.target.value }
+              })
+            }}
+          />
         </div>
 
         <div className="action-buttons">
