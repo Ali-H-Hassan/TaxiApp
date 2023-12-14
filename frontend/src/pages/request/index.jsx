1e5c71051a12ff5f677c6817a2fc4bafe8cd6838
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2023-12-14 12:40:17 +0200

    axios

--- frontend/src/pages/request/index.jsx
+++ frontend/src/pages/request/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './index.css'
 import { MyMap } from './MyMap'
+import axios from 'axios'
 
 const RequestRide = () => {
+  // try {
+  //     const res = await axios.post(
+  //       'http://127.0.0.1:8000/api/create_ride',
+  //       {
+  //         passenger_id: credentials.id,
+  //         start_location: credentials.start_location,
+  //         end_location: credentials.end_location,
+  //         start_time:credentials.start_time
+  //       },
+  //       {
+  //         headers: {
+  //           Accept: 'application/json',
+  //           'Content-Type': 'application/json'
+  //         }
+  //       }
+  //     )
   return (
     <div className="request-ride-container">
       <div className="ride-form">
@@ -16,6 +33,7 @@ const RequestRide = () => {
             <div className="input-container">
               <div className="input-box">
                 <div className="input-label">Pick up location</div>
+                {/* Add your Input component here if needed */}
               </div>
             </div>
           </div>
@@ -23,6 +41,7 @@ const RequestRide = () => {
             <div className="input-container">
               <div className="input-box">
                 <div className="input-label">Destination</div>
+                {/* Add your Input component here if needed */}
               </div>
             </div>
           </div>
@@ -30,6 +49,7 @@ const RequestRide = () => {
         <div className="login-button">
           <div className="button-container">
             <div className="button-text">Login</div>
+            {/* Add your Button component here if needed */}
           </div>
         </div>
       </div>
